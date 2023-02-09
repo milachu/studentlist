c3f08d462af14a94e6e44ae09bd2af60b0bd1087
milachu <chuxi@sheridancollege.ca>
2023-02-09 06:32:20 -0500

    first commit

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,17 @@
+package studentlist;
+
+/**
+ *This class +++ insert class description here +++
+ *
+ * @author Xiaoxu Chu
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
